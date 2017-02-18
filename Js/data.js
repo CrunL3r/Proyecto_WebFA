77e251eb547d7056e0f81e87a0f076fcd21bdafd
Andreu <andreullabresb@gmail.com>
2017-02-18 14:16:49 +0100

    Update JS && CSS
    - Added new images in the array.
    - Add Added banner.
    - Solucionado error logo de la web.

--- Js/data.js
+++ Js/data.js
@@ -1,96 +1,96 @@
 var procesors = [
 {
-"img" : "",
-"name": "I5 4ghz",
+"img" : "Img/Articulos/Procesors/I3.png",
+"name": "Intel I3 2,3ghz",
+"cost": 110
+},
+{
+"img" : "Img/Articulos/Procesors/I5.png",
+"name": "Intel I5 3,2ghz",
+"cost": 280
+},
+{
+"img" : "Img/Articulos/Procesors/I7.png",
+"name": "Intel I7 4,7ghz",
+"cost": 360
+},
+{
+"img" : "Img/Articulos/Procesors/AmdFX1.png",
+"name": "AMD FX 4.8ghz",
 "cost": 300
 },
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
+"img" : "Img/Articulos/Procesors/AmdA.png",
+"name": "AMD AM 3ghz",
+"cost": 100
 },
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
-},
-{
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
-},
-{
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
-},
-{
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
+"img" : "Img/Articulos/Procesors/AMDFXEX.png",
+"name": "AMD FX Extreme 4.7ghz",
+"cost": 350
 }
-]
+] // Campo de Procesadores.
 var motherboards = [
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 200
+"img" : "Img/Articulos/motherboards/Asus_a.png",
+"name": "Asus ROG Maximus",
+"cost": 230
 },
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
+"img" : "Img/Articulos/motherboards/Asus_R.png",
+"name": "MSI Gaming",
+"cost": 174
 },
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
+"img" : "Img/Articulos/motherboards/Asus_R2.png",
+"name": "Asus ROG Formula",
+"cost": 175
 },
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
+"img" : "Img/Articulos/motherboards/Giga_G.png",
+"name": "Gigabyte Gaming",
+"cost": 154
 },
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
+"img" : "Img/Articulos/motherboards/Placa_asus_b.png",
+"name": "Asus Basic",
+"cost": 64
 },
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
+"img" : "Img/Articulos/motherboards/Msi_G.png",
+"name": "MSI basic",
+"cost": 50
 }
-]
+] //Campo de Placas Base.
 var grafics = [
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 500
+"img" : "Img/Articulos/Grafics/gtx-960.png",
+"name": "NVIDIA GTX 960",
+"cost": 210
 },
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
+"img" : "Img/Articulos/Grafics/gtx-970.png",
+"name": "NVIDIA GTX 970",
+"cost": 347
 },
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
+"img" : "Img/Articulos/Grafics/R9-Fury-X.png",
+"name": "AMD Fury X",
+"cost": 430
 },
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
+"img" : "Img/Articulos/Grafics/R9_Nano.png",
+"name": "AMD Fury Nano",
+"cost": 280
 },
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
+"img" : "Img/Articulos/Grafics/gtx-1070.png",
+"name": "NVIDIA GTX 1070",
+"cost": 390
 },
 {
-"img" : "",
-"name": "I5 4ghz",
-"cost": 300
+"img" : "Img/Articulos/Grafics/gtx_1080.png",
+"name": "NVIDIA GTX 1080",
+"cost": 440
 }
-]
+] //Campo de Tarjetas Graficas.
--- Js/funtions.js
+++ Js/funtions.js
@@ -1,16 +1,16 @@
-function ShowTable(array)
+function ShowTable(array) //Sirve para cualquier campo.
 {
     var html = "";
     var m = 0;
     
-    html = `<table style="width:25%">`;
+    html = `<table style="width:100%">`;
     for (let i = 0; i < array.length/3; i++)
     {
         var n = 0;
         html += `<tr>`;
         while (n < 3 && m < array.length)
         {
-            html += `<th> ${array[m].name} <br> <img href = "${array[m].img}"> <br> ${array[m].cost}€ </th>`;
+            html += `<th> ${array[m].name} <br> <img src = "${array[m].img}"> <br> ${array[m].cost}€ </th>`;
             m++;
             n++;
         }
@@ -20,33 +20,56 @@ function ShowTable(array)
     document.getElementById("table").innerHTML = html;
 }
 
-function PriceConfig() //Calculo de datos. config
+function PriceConfig() //Calculo de precio, si se añaden campos en data hay que modificarlo.
 {
+    var Result = 0; //Coste.
+    var Field = 3; //Num de campos que hay.
+    var Config = new Array(); //Datos introducidos.
+    for (let i = 0; i < Field; i++)
+    {
+        Config[i] = document.getElementById(`${i}`).value;
+    }
+    if (Config[0] != procesors.length)
+    {
+        Result = Result + procesors[Config[0]].cost;  
+    }
+    if (Config[1] != motherboards.length)
+    {
+        Result = Result + motherboards[Config[1]].cost;  
+    }
+    if (Config[2] != grafics.length)
+    {
+        Result = Result + grafics[Config[2]].cost;
+    }
+    document.getElementById("exit").value = Result;
 }
 
-function CreateConnfigurator(array1, array2, array3)
+function CreateConnfigurator(array1, array2, array3) //Si hay nuevos campos en data hay que modificarlo.
 {
     var html = "";
     
     html = `<form>`;
-    html += `<select id="lista1">`;
+    html += `Seleccionar un procesador: <select id="0">`;
     for (let i = 0; i < array1.length; i++)
     {
-        html += `<option value= 0>${array1[i].name}</option>`;
+        html += `<option value= ${i}>${array1[i].name}</option>`;
     }
     html += `<option value= ${array1.length} selected>-</option>
-    </select> <br> <select id="lista1">`;
+    </select> <br> Seleccionar una placa base: <select id="1">`;
     for (let i = 0; i < array2.length; i++)
     {
-        html += `<option value= 0>${array2[i].name}</option>`;
+        html += `<option value= ${i}>${array2[i].name}</option>`;
     }
         html += `<option value= ${array2.length} selected>-</option>
-    </select> <br> <select id="lista1">`;
+    </select> <br> Seleccionar una tarjeta gráfica: <select id="2">`;
     for (let i = 0; i < array3.length; i++)
     {
-        html += `<option value= 0>${array3[i].name}</option>`; 
+        html += `<option value= ${i}>${array3[i].name}</option>`; 
     }
     html += `<option value= ${array3.length} selected>-</option>
-    </select> <br> </form>`;
-    document.getElementById("formulario").innerHTML = html;
+    </select> <br> 
+    <input type = "button" value = "Calcular" onclick = "PriceConfig()"> 
+    <input type="text" size="20" id = "exit">
+    </form>`;
+    document.getElementById("Formulario").innerHTML = html;
 }
